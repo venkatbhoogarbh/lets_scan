1e88268fc328537f963830dacc797448a97d6c1e
Venkat Bhoogarbh <venkatbhoogarbh@gmail.com>
2025-12-12 18:49:48 +0530

    Added code to script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+function onScanSuccess(decodedText, decodedResult) {
+    console.log(Code scanned = ${decodedText});
+    document.getElementById("result").innerText = decodedText;
+}
 
+function onScanFailure(error) {
+    // ignore errors (happens when scanning)
+}
+
+let scanner = new Html5QrcodeScanner(
+    "reader",
+    {
+        fps: 10,
+        qrbox: 250
+    });
+
+scanner.render(onScanSuccess, onScanFailure);
